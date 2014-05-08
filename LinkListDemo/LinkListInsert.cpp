c1036c7ac546c8ef0c51d10ddbc61b4cbff1e6ba
ashish-khanna <khanna@ccs.neu.edu>
2014-05-08 08:22:56 -0400

    Link List Updated

--- LinkListDemo/LinkListInsert.cpp
+++ LinkListDemo/LinkListInsert.cpp
@@ -24,60 +24,144 @@ class linked
 		start=NULL;
 	}
 
-
-	void insert_at_end()
-	{
-		node *temp;
-
-		if(start==NULL)
-		{
-			start=new node [1];
-			start->p=NULL;
-			cout<<"\n Enter Data to insert \n";
-			cin>>start->d;
-		}
-		else
-		{
-			temp=start;
-			while(temp->p!=NULL)
-				temp=temp->p;
-
-			temp->p= new node [1];
-			temp=temp->p;
-
-			cout<<"\n Enter Data to insert \n";
-			cin>>temp->d;
-			temp->p=NULL;
-		}
-	}
-
-	void display()
-	{
-		node *temp;
-		temp=start;
-
-		cout<<"\n Linked List elements are \n";
-
-		while(temp->p!=NULL)
-		{
-			cout<<""<<temp->d<<" ";
-			temp=temp->p;
-		}
-		cout<<temp->d<<endl;
-	}
-
+	void insert_at_end();
+	void insert_at_mid();
+	void display();
+	void insert_at_beg();
+	int remove();
 };
 
+void linked :: insert_at_end()
+{
+	node *temp;
+
+	if(start==NULL)
+	{
+		start=new node [1];
+		start->p=NULL;
+		cout<<"\n Enter Data to insert \n";
+		cin>>start->d;
+	}
+	else
+	{
+		temp=start;
+		while(temp->p!=NULL)
+			temp=temp->p;
+		temp->p= new node [1];
+		temp=temp->p;
+
+		cout<<"\n Enter Data to insert \n";
+		cin>>temp->d;
+		temp->p=NULL;
+	}
+}
+
+void linked :: display()
+{
+	node *temp;
+	temp=start;
+
+	cout<<"\n Linked List elements are \n";
+
+	while(temp->p!=NULL)
+	{
+		cout<<""<<temp->d<<" ";
+		temp=temp->p;
+	}
+	cout<<temp->d<<endl;
+}
+
+
+void linked :: insert_at_mid()
+{
+	node *temp,*next;
+	int x;
+
+	temp=start;
+	cout<<"\n Enter the Neighbour element \n";
+	cin>>x;
+
+	while(temp->d!=x)
+		temp=temp->p;
+
+	next=temp->p;
+	temp->p=new node [1];
+	temp=temp->p;
+	temp->p=next;
+	cout<<"\n Enter data to insert \n";
+	cin>>temp->d;
+}
+
+void linked :: insert_at_beg()
+{
+	node *temp;
+	temp=new node [1];
+	temp->p=start;
+	start=temp;
+
+	cout<<"\n Enter element to insert \n";
+	cin>>temp->d;
+}
+
+int linked :: remove()
+{
+	node *temp,*prev;
+	int x;
+
+	if(start==NULL)
+	{
+		cout<<"\n Underflow -- No element to Delete \n";
+		return 0;
+	}
+
+	cout<<"\n Enter element to delete \n";
+	cin>>x;
+
+	if(start->d==x && start->p==NULL)
+	{
+		delete start;
+		start=NULL;
+		cout<<"\n Deletion successfull \n";
+		return 0;
+	}
+	if(start->d==x)
+	{
+		temp=start->p;
+		delete start;
+		start=temp;
+		cout<<"\n Deletion Successfull \n";
+		return 0;
+	}
+	temp=start;
+	while(temp->d!=x)
+	{
+		prev=temp;
+		temp=temp->p;
+	}
+	prev->p=temp->p;
+	cout<<"\n Deletion Successfull \n";
+	return x;
+}
 
 int main()
 {
-	int n=0,a;
 	linked l;
 
 	l.insert_at_end();
 	l.insert_at_end();
 	l.insert_at_end();
 	l.insert_at_end();
+
+	l.insert_at_mid();
+	l.insert_at_mid();
+
+	l.insert_at_beg();
+	l.insert_at_beg();
+
+	l.display();
+
+	l.remove();
+	l.remove();
 
 	l.display();
 	return 0;
